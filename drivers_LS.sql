d364894334bebd7445b0e3a82cb468043d3c3fc5
staicu <staicu4@gmail.com>
2017-01-30 22:40:05 -0500

    added files to project

---
+++ drivers_LS.sql
@@ -0,0 +1,22 @@
+/* Create table Drivers*/
+create table Drivers (
+
+
+DriverID	
+DriverFirst	
+DriverLast	
+SSN	
+DOB	
+StartDate	
+EndDate	
+Address	
+City	
+State	
+Zip	
+Phone	
+Cell	
+MileageRate	
+Review	
+DrivingRecord
+
+)
